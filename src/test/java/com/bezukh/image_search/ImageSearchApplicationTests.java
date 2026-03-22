683dd63abdc648e992e4ac13384030bf9bab906f
Danylo Bezukh <113092192+DaniloBez@users.noreply.github.com>
2026-03-22 16:30:24 +0200

    Initial commit

---
+++ src/test/java/com/bezukh/image_search/ImageSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bezukh.image_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
